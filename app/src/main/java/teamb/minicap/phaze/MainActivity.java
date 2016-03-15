f18a0519bbe247d5050f1ec87701b930a0bd02ee
ArsalanAli <arsalanali18@gmail.com>
2016-03-15 12:59:09 -0400

    merged changes alex made, had to do it mauanl to resolve some conflict

--- app/src/main/java/teamb/minicap/phaze/MainActivity.java
+++ app/src/main/java/teamb/minicap/phaze/MainActivity.java
@@ -1,13 +1,16 @@
 package teamb.minicap.phaze;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.AudioManager;
 import android.net.Uri;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -42,6 +45,25 @@ public class MainActivity extends AppCompatActivity {
     Button gbutton;
     Button sbutton;
     Hub hub;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private AudioManager MuteManager;
+
+    private boolean headsetConnected = false;
+
+    /*public void onReceive(Context context, Intent intent) {
+        if (intent.hasExtra("state")){
+            if (headsetConnected && intent.getIntExtra("state", 0) == 0){
+                headsetConnected = false;
+            } else if (!headsetConnected && intent.getIntExtra("state", 0) == 1){
+                headsetConnected = true;
+            }
+        }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +102,10 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        MuteManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
     }
 
     /*IDs for everything on main activity
@@ -93,6 +119,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://teamb.minicap.phaze/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
     }
     @Override
     public void onResume(){
@@ -100,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
         String theme = prefs.getString("Themes", "Default");
         switch (theme) {
             case "Dark":
-                //themechange(2);
+                themechange(2);
                 break;
             case "Light":
                 themechange(1);
@@ -109,8 +151,22 @@ public class MainActivity extends AppCompatActivity {
                 themechange(0);
                 break;
         }
+        String on_unplug = prefs.getString("headset_on_unplug", "Nothing");
+        switch (on_unplug) {
+            case "Nothing":
+                //do nothing
+                break;
+            case "Mute":
+                //Muteme;
+                //MuteManager.adjustSuggestedStreamVolume(AudioManager.ADJUST_LOWER,AudioManager.STREAM_MUSIC,100);
+                break;
+            case "Pause":
+                //Pauseme;
+                break;
+        }
     }
-    public void themechange(int i){
+
+    public void themechange(int i) {
         Boolean connected = prefs.getBoolean("Connected", false);
         int button_text = 0;
         int button_active = 0;
@@ -122,53 +178,64 @@ public class MainActivity extends AppCompatActivity {
                 button_active = R.drawable.button_active;
                 button_inactive = R.drawable.button_inactive;
                 break;
+            //this is the dark, original theme
             case 1:
                 findViewById(R.id.main).setBackgroundColor(Color.argb(255, 51, 181, 229));
                 button_text = R.color.button_text_light;
                 button_active = R.drawable.button_active_light;
                 button_inactive = R.drawable.button_inactive_light;
                 break;
+            //this is the light, blue theme
             case 2:
+                findViewById(R.id.main).setBackgroundColor(Color.argb(255, 66, 68, 69));
+                button_text = R.color.button_text_dark;
+                button_active = R.drawable.button_active_dark;
+                button_inactive = R.drawable.button_inactive_dark;
                 break;
+            //this is the dark, blacker theme
         }
-        if (android.os.Build.VERSION.SDK_INT >= 23) {
-            cbutton.setTextColor(ContextCompat.getColor(this,button_text));
-            sbutton.setTextColor(ContextCompat.getColor(this,button_text));
-            mbutton.setTextColor(ContextCompat.getColor(this,button_text));
-            vbutton.setTextColor(ContextCompat.getColor(this,button_text));
-            gbutton.setTextColor(ContextCompat.getColor(this,button_text));
-        }
-        else {
+        if (Build.VERSION.SDK_INT >= 23) {
+            cbutton.setTextColor(ContextCompat.getColor(this, button_text));
+            sbutton.setTextColor(ContextCompat.getColor(this, button_text));
+            mbutton.setTextColor(ContextCompat.getColor(this, button_text));
+            vbutton.setTextColor(ContextCompat.getColor(this, button_text));
+            gbutton.setTextColor(ContextCompat.getColor(this, button_text));
+        } else {
             cbutton.setTextColor(getResources().getColor(button_text));
             sbutton.setTextColor(getResources().getColor(button_text));
             mbutton.setTextColor(getResources().getColor(button_text));
             vbutton.setTextColor(getResources().getColor(button_text));
             gbutton.setTextColor(getResources().getColor(button_text));
         }
-        if(android.os.Build.VERSION.SDK_INT >= 21){
+        if (Build.VERSION.SDK_INT >= 21) {
             cbutton.setBackground(getResources().getDrawable(button_active, null));
             sbutton.setBackground(getResources().getDrawable(button_active, null));
-            if (connected){
+            if (connected) {
                 mbutton.setEnabled(true);
                 vbutton.setEnabled(true);
                 gbutton.setEnabled(true);
-                mbutton.setBackground(getResources().getDrawable(button_active,null));
-                vbutton.setBackground(getResources().getDrawable(button_active,null));
-                gbutton.setBackground(getResources().getDrawable(button_active,null));
+                mbutton.setBackground(getResources().getDrawable(button_active, null));
+                vbutton.setBackground(getResources().getDrawable(button_active, null));
+                gbutton.setBackground(getResources().getDrawable(button_active, null));
+            } else {
+                mbutton.setEnabled(true);
+                vbutton.setEnabled(true);
+                gbutton.setEnabled(true);
+                mbutton.setBackground(getResources().getDrawable(button_active, null));
+                vbutton.setBackground(getResources().getDrawable(button_active, null));
+                gbutton.setBackground(getResources().getDrawable(button_active, null));
             }
-            else{
-                mbutton.setEnabled(false);
-                vbutton.setEnabled(false);
-                gbutton.setEnabled(false);
-                mbutton.setBackground(getResources().getDrawable(button_inactive,null));
-                vbutton.setBackground(getResources().getDrawable(button_inactive,null));
-                gbutton.setBackground(getResources().getDrawable(button_inactive,null));
-            }
-        }
-        else{
+        } else {
             cbutton.setBackground(getResources().getDrawable(button_active));
             sbutton.setBackground(getResources().getDrawable(button_active));
-            if (connected){
+            if (connected) {
+                mbutton.setEnabled(true);
+                vbutton.setEnabled(true);
+                gbutton.setEnabled(true);
+                mbutton.setBackground(getResources().getDrawable(button_active));
+                vbutton.setBackground(getResources().getDrawable(button_active));
+                gbutton.setBackground(getResources().getDrawable(button_active));
+            } else {
                 mbutton.setEnabled(true);
                 vbutton.setEnabled(true);
                 gbutton.setEnabled(true);
@@ -176,16 +243,23 @@ public class MainActivity extends AppCompatActivity {
                 vbutton.setBackground(getResources().getDrawable(button_active));
                 gbutton.setBackground(getResources().getDrawable(button_active));
             }
-            else{
-                mbutton.setEnabled(false);
-                vbutton.setEnabled(false);
-                gbutton.setEnabled(false);
-                mbutton.setBackground(getResources().getDrawable(button_inactive));
-                vbutton.setBackground(getResources().getDrawable(button_inactive));
-                gbutton.setBackground(getResources().getDrawable(button_inactive));
+        }
+    }
+/*  this is alex's stuff for the mute and pause settings
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.hasExtra("state")){
+            if (headsetConnected && intent.getIntExtra("state", 0) == 0){
+                Music.setVolume((float) 0);
+                headsetConnected = false;
+
+            } else if (!headsetConnected && intent.getIntExtra("state", 0) == 1){
+                headsetConnected = true;
+                myActivity.music.setVolume((float) 1);
             }
         }
     }
+*/
     public void connect(View view) {
         Intent intent = new Intent(MainActivity.this, ScanActivity.class);
         startActivity(intent);
@@ -287,27 +361,23 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_music, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (R.id.myosdk__action_scan == id) {
-            onScanActionSelected();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    public void onStop() {
+        super.onStop();
 
-    private void onScanActionSelected() {
-        // Launch the ScanActivity to scan for Myos to connect to.
-        Intent intent = new Intent(this, ScanActivity.class);
-        startActivity(intent);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://teamb.minicap.phaze/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
-
 }
 
