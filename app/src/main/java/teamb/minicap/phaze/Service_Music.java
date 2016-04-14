4cd66d3e408110c13c5704be54a6fca6ea38cb99
alexroy514 <alexroy.514@gmail.com>
2016-04-14 00:10:29 -0400

    fade in works! woop woop took forever, longest open activity on taiga :O

--- app/src/main/java/teamb/minicap/phaze/Service_Music.java
+++ app/src/main/java/teamb/minicap/phaze/Service_Music.java
@@ -6,15 +6,20 @@ import java.util.ArrayList;
 import android.app.Service;
 import android.content.ContentUris;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Notification;
 import android.app.PendingIntent;
 
@@ -38,6 +43,14 @@ public class Service_Music extends Service implements
     private static final int NOTIFY_ID = 1;
     private boolean shuffle=false;
     private Random rand;
+
+    //needed for fade in
+    private int iVolume;
+    private final static int INT_VOLUME_MAX = 15;
+    private final static int INT_VOLUME_MIN = 0;
+    private final static float FLOAT_VOLUME_MAX = 1;
+    private final static float FLOAT_VOLUME_MIN = 0;
+
 
     public void onCreate(){
         //create the service
@@ -133,6 +146,39 @@ public class Service_Music extends Service implements
             Log.e("MUSIC SERVICE", "Error setting data source", e);
         }
 
+        //Set current volume, depending on fade or not
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int fadeDuration = prefs.getInt("fadeseekbar", 5000);
+
+
+        if (fadeDuration > 0)
+            iVolume = INT_VOLUME_MIN;
+        else
+            iVolume = INT_VOLUME_MAX;
+
+        updateVolume(0);
+
+        //Start increasing volume in increments
+        if(fadeDuration > 0) {
+            final Timer timer = new Timer(true);
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    updateVolume(1);
+                    if (iVolume == INT_VOLUME_MAX) {
+                        timer.cancel();
+                        timer.purge();
+                    }
+                }
+            };
+
+            // calculate delay, cannot be zero, set to 1 if zero
+            int delay = fadeDuration / INT_VOLUME_MAX;
+            if (delay == 0) delay = 1;
+
+            timer.schedule(timerTask, delay, delay);
+        }
+
         player.prepareAsync();
     }
 
@@ -188,5 +234,27 @@ public class Service_Music extends Service implements
     @Override
     public void onDestroy() {
         stopForeground(true);
+    }
+    private void updateVolume(int change)
+    {
+        //increment or decrement depending on type of fade
+        iVolume = iVolume + change;
+
+        //ensure iVolume within boundaries
+        if (iVolume < INT_VOLUME_MIN)
+            iVolume = INT_VOLUME_MIN;
+        else if (iVolume > INT_VOLUME_MAX)
+            iVolume = INT_VOLUME_MAX;
+
+        //convert to float value
+        float fVolume = 1 - ((float) Math.log(INT_VOLUME_MAX - iVolume) / (float) Math.log(INT_VOLUME_MAX));
+
+        //ensure fVolume within boundaries
+        if (fVolume < FLOAT_VOLUME_MIN)
+            fVolume = FLOAT_VOLUME_MIN;
+        else if (fVolume > FLOAT_VOLUME_MAX)
+            fVolume = FLOAT_VOLUME_MAX;
+
+        player.setVolume(fVolume, fVolume);
     }
 }
